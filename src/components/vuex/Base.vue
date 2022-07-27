1631a8544dede7c1bb23d93ed974dba4218fc7c3
Егор <eremin.e@m-style.spb.ru>
2022-07-27 18:00:40 +0300

    still learning vue

--- src/components/vuex/Base.vue
+++ src/components/vuex/Base.vue
@@ -5,12 +5,14 @@
         <th>имя</th>
         <th>что это</th>
         <th>Зачем это</th>
+        <th>Как это</th>
       </thead>
       <tbody>
         <tr v-for="item in items" :key="item">
           <td>{{ item.name }}</td>
           <td>{{ item.title }}</td>
           <td>{{ item.description }}</td>
+          <td>{{ item.how }}</td>
         </tr>
       </tbody>
     </table>
@@ -23,26 +25,31 @@ const BASE_THINGS = [
     name: "state",
     title: "Состояние",
     description: "куда уж без него",
+    how: '',
   },
   {
     name: "getters",
     title: "Просто гетеры",
     description: "Действия которые возвращают данные из хранилища",
+    how: '',
   },
   {
     name: "mutations",
     title: "Синхронные изменения состояния",
     description: "Синхронные транзакции",
+    how: 'this.$store.commit("increment")',
   },
   {
     name: "actions",
     title: "Действия — похожи на мутации с несколькими отличиями",
     description: "Вместо того, чтобы напрямую менять состояние, действия инициируют мутации. Действия могут использоваться для асинхронных операций.",
+    how: 'this.$store.dispatch("increment")',
   },
   {
     name: "modules",
     title: "ну модули там...",
     description: "не дочитал еще",
+    how: '',
   },
 ];
 export default {
